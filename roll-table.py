6ef37ca0e5a6183046f6a73cb2d6fb264bc5a7ae
Stephen Faure <faure.stephen@gmail.com>
2023-08-09 01:07:10 +0200

    Add option for dice roll formula

--- roll-table.py
+++ roll-table.py
@@ -5,24 +5,63 @@ import os.path
 import argparse
 from pathlib import Path
 import random
+import dice
+import types
 
-LOCAL_ARGS = None
 
-
-class table_roller:
-    def __init__(self, table_filepath):
-        with Path(table_filepath).open("r") as table_file:
+class TableRoller:
+    def __init__(self, config):
+        with Path(config.table_filepath).open("r") as table_file:
             table_content = table_file.read()
             self.table = [line for line in table_content.split("\n") if line]
 
-    def get_table(self):
-        return self.table
+            self.roll_config = types.SimpleNamespace()
+            self.roll_config.count = config.count
+            self.roll_config.exclusive = config.exclusive
 
-    def roll_result(self):
-        if LOCAL_ARGS.exclusive:
-            return random.sample(self.table, k=min(LOCAL_ARGS.count, len(self.table)))
+            if not config.formula:
+                self.roll_config.formula = None
+                return
+            else:
+                self.roll_config.formula = f"{config.formula}t"
+
+            if (
+                dice.roll_max(self.roll_config.formula) > len(self.table)
+                or dice.roll_min(self.roll_config.formula) < 0
+            ):
+                raise IndexError(
+                    """The supplied dice formula should not roll higher than the number of entries on the table or lower than 0"""
+                )
+
+    def get_default_result(self):
+        if self.roll_config.exclusive:
+            return random.sample(
+                self.table, k=min(self.roll_config.count, len(self.table))
+            )
         else:
-            return random.choices(self.table, k=LOCAL_ARGS.count)
+            return random.choices(self.table, k=self.roll_config.count)
+
+    def get_formula_result(self):
+        results = [
+            self.table[dice.roll(self.roll_config.formula) - 1]
+            for _ in range(self.roll_config.count)
+        ]
+        return results
+
+    def get_result(self):
+        if self.roll_config.formula:
+            return self.get_formula_result()
+        else:
+            return self.get_default_result()
+
+
+def print_results(result_array, output, append):
+    if output:
+        output_action = "a" if append else "w"
+        with Path(output).open(output_action) as output:
+            [print(result, file=output) for result in result_array]
+    else:
+        [print(result) for result in result_array]
 
 
 def usage():
@@ -38,18 +77,15 @@ def get_parameters():
                     a configuration file and rolls a random result from it""",
     )
 
-    parser.add_argument(
-        "table_filepath", help="path to random table config file")
+    parser.add_argument("table_filepath", help="path to random table config file")
 
     output_group = parser.add_argument_group("Output Options")
-
     output_group.add_argument(
         "-o",
         "--output",
         help="""Text file to output the rolled results.
                 Note: Contents will be overwritten.""",
     )
-
     output_group.add_argument(
         "-a",
         "--append",
@@ -74,26 +110,24 @@ def get_parameters():
         action="store_true",
         help="Each result can be rolled at most once",
     )
+    roll_group.add_argument(
+        "-f",
+        "--formula",
+        help="""Custom dice formula to roll on the table.
+                Keep it simple for now (XdY±Z)""",
+    )
 
-    global LOCAL_ARGS
-    LOCAL_ARGS = parser.parse_args()
+    args = parser.parse_args()
 
-    if not os.path.isfile(LOCAL_ARGS.table_filepath):
-        logging.error(f"File '{LOCAL_ARGS.table_file}' not found.")
+    if not os.path.isfile(args.table_filepath):
+        logging.error(f"File '{args.table_filepath}' not found.")
         usage()
         exit(2)
 
-
-def print_results(result_array):
-    if LOCAL_ARGS.output:
-        output_action = "a" if LOCAL_ARGS.append else "w"
-        with Path(LOCAL_ARGS.output).open(output_action) as output:
-            [print(result, file=output) for result in result_array]
-    else:
-        [print(result) for result in result_array]
+    return args
 
 
 if __name__ == "__main__":
-    get_parameters()
-    table_roller = table_roller(LOCAL_ARGS.table_filepath)
-    print_results(table_roller.roll_result())
+    args = get_parameters()
+    table_roller = TableRoller(args)
+    print_results(table_roller.get_result(), args.output, args.append)
